324e66b874bf4702d9ab8d72e83a1e2082cbcee2
Barton Hammond <barton.hammond@support.com>
2019-09-10 14:36:52 -0500

    more tests

--- app/core/models.py
+++ app/core/models.py
@@ -8,10 +8,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("Users must have email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
